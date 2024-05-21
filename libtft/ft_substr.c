5654d11d06f2420830e101b160950d006f3e54af
Piotr Wojnarowski <pwojnaro@1-A-31.42heilbronn.de>
2024-05-21 20:38:41 +0200

    Libft error norminet fix

--- libtft/ft_substr.c
+++ libtft/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: pwojnaro <pwojnaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 16:59:21 by pwojnaro          #+#    #+#             */
-/*   Updated: 2024/03/16 20:53:05 by pwojnaro         ###   ########.fr       */
+/*   Updated: 2024/05/21 20:36:48 by pwojnaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,3 @@ char	*ft_strdup(char const *src)
 	dest[i] = '\0';
 	return (dest);
 }
-
