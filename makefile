83825013046e95c9b37fae65e7e518c3e32ace42
Piotr Wojnarowski <pwojnaro@1-A-31.42heilbronn.de>
2024-07-21 13:17:33 +0200

    makefile correction

--- makefile
+++ makefile
@@ -1,16 +1,13 @@
 # Compiler and flags
 CC = cc
 CFLAGS = -Wall -Werror -Wextra 
-
-# Project name
-NAME = push_swap
+LDFLAGS = -Llibft -lft
 
 # Directories
-SRCDIR = src
-LIBFTDIR = ./libtft
+CFLAGS = -I./include -I./libft -Wall -Wextra -Werror
 
 # Source files
-SRCS = $(SRCDIR)/stack_operations1.c \
+SOURCES = $(SRCDIR)/stack_operations1.c \
        $(SRCDIR)/stack_operations2.c \
        $(SRCDIR)/stack_operations3.c \
        $(SRCDIR)/stack_operations4.c \
@@ -22,36 +19,36 @@ SRCS = $(SRCDIR)/stack_operations1.c \
 	   $(SRCDIR)/error_handling.c \
        $(SRCDIR)/main.c
 
+BUILD_DIR = build
+
 # Object files
-OBJS = $(SRCS:.c=.o)
+OBJECTS = $(SOURCES:%.c=$(BUILD_DIR)/%.o)
 
-# Include directories
-INCLUDES = -I$(LIBFTDIR)
+EXECUTABLE = push_swap
 
-# Library file
-LIBFT = $(LIBFTDIR)/libft.a
+# Default target
+all: $(EXECUTABLE)
 
-# Targets
-all: $(NAME)
+# Rule to create the executable
+$(EXECUTABLE): $(OBJECTS) libft/libft.a
+	$(CC) $(OBJECTS) -o $@ $(LDFLAGS)
 
-$(NAME): $(LIBFT) $(OBJS)
-	$(CC) $(CFLAGS) -o $(NAME) $(OBJS) -L$(LIBFTDIR) -lft
+# Rule to create object files in the build directory
+$(BUILD_DIR)/%.o: %.c
+	@mkdir -p $(BUILD_DIR)
+	$(CC) $(CFLAGS) -c $< -o $@
 
-$(LIBFT):
-	@$(MAKE) -C $(LIBFTDIR)
-	@echo "Library libft.a built successfully."
+# Ensure libft is built
+libft/libft.a:
+	make -C libft all
 
-%.o: %.c
-	$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
-
+# Clean up build files
 clean:
-	@rm -f $(OBJS)
-	@$(MAKE) -C $(LIBFTDIR) clean
+	rm -f $(OBJECTS) $(EXECUTABLE)
+	make -C libft clean
 
 fclean: clean
-	@rm -f $(NAME)
-	@$(MAKE) -C $(LIBFTDIR) fclean
+	rm -f $(EXECUTABLE)
+	make -C libft fclean
 
 re: fclean all
-
-.PHONY: all clean fclean re
---
+++ stack_operations5.c
@@ -0,0 +1,102 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_operations5.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pwojnaro <pwojnaro@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/21 13:07:21 by pwojnaro          #+#    #+#             */
+/*   Updated: 2024/07/21 13:07:38 by pwojnaro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	align_stack_a(t_stack_node **a, t_stack_node *target_node)
+{
+	while (*a != target_node)
+	{
+		if (target_node->median)
+			ra(a);
+		else
+			rra(a);
+	}
+}
+
+void	align_stack_b(t_stack_node **b, t_stack_node *cheapest)
+{
+	while (*b != cheapest)
+	{
+		if (cheapest->median)
+			rb(b);
+		else
+			rrb(b);
+	}
+}
+
+void	align(t_stack_node **a, t_stack_node **b, t_nodes *nodes)
+{
+	if (nodes->cheap->median && nodes->node->median)
+	{
+		while (*a != nodes->node && *b != nodes->cheap)
+			rr(a, b);
+	}
+	else if (!nodes->cheap->median && !nodes->node->median)
+	{
+		while (*a != nodes->node && *b != nodes->cheap)
+			rrr(a, b);
+	}
+}
+
+void	move_opt_node(t_stack_node **a, t_stack_node **b)
+{
+	t_stack_node	*cheapest;
+	t_stack_node	*target_node;
+
+	cheapest = return_cheapest(*b);
+	target_node = cheapest->target_node;
+	while (*b != cheapest)
+	{
+		if (cheapest->median)
+			rb(b);
+		else
+			rrb(b);
+	}
+	while (*a != target_node)
+	{
+		if (target_node->median)
+			ra(a);
+		else
+			rra(a);
+	}
+	pa(a, b);
+}
+
+void	set_target_node(t_stack_node *a, t_stack_node *b)
+{
+	t_stack_node	*smallest_node;
+	t_stack_node	*current_a;
+	t_stack_node	*target_node;
+
+	smallest_node = return_smallest(a);
+	while (b)
+	{
+		target_node = NULL;
+		current_a = a;
+		while (current_a)
+		{
+			if (current_a->node > b->node)
+			{
+				if (!target_node || current_a->node < target_node->node)
+				{
+					target_node = current_a;
+				}
+			}
+			current_a = current_a->fwd;
+		}
+		if (!target_node)
+			target_node = smallest_node;
+		b->target_node = target_node;
+		b = b->fwd;
+	}
+}
