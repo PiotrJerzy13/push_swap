1b376739f2933849037fac0bc4d4a1be97398892
Piotr Wojnarowski <pwojnaro@1-B-31.42heilbronn.de>
2024-07-24 18:49:47 +0200

    rearranging and renaming

--- stack_operations.c
+++ stack_operations.c
@@ -6,112 +6,60 @@
 /*   By: pwojnaro <pwojnaro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 14:05:25 by pwojnaro          #+#    #+#             */
-/*   Updated: 2024/07/22 12:54:04 by pwojnaro         ###   ########.fr       */
+/*   Updated: 2024/07/24 18:47:43 by pwojnaro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	append_node(t_stack_node **a, t_stack_node *new_node)
+void	push_stack(t_stack_node **dst, t_stack_node **src)
 {
-	t_stack_node	*last;
+	t_stack_node	*number_to_push;
 
-	last = *a;
-	while (last->fwd)
-		last = last->fwd;
-	last->fwd = new_node;
-	new_node->bwd = last;
-}
-
-int	init_stack_no_split(t_stack_node **a, int argc, char **argv)
-{
-	int				i;
-	long			val;
-	t_stack_node	*new_node;
-
-	i = 1;
-	while (i < argc)
-	{
-		val = ft_atol(argv[i]);
-		if (!handle_errors(a, argv[i], val))
-		{
-			free_errors(a, NULL, argc);
-		}
-		new_node = create_node(val);
-		if (!new_node)
-		{
-			free_errors(a, NULL, argc);
-		}
-		if (!(*a))
-			*a = new_node;
-		else
-			append_node(a, new_node);
-		i++;
-	}
-	return (1);
-}
-
-long	ft_atol(const char *str)
-{
-	int		pos_sign;
-	long	output;
-
-	output = 0;
-	pos_sign = 1;
-	while ((*str >= 9 && *str <= 13) || *str == 32)
-		str++;
-	if (*str == '+' || *str == '-')
-	{
-		if (*str == '-')
-			pos_sign = -1;
-		str++;
-		if (*str == '+' || *str == '-')
-			return (0);
-	}
-	while (*str >= '0' && *str <= '9')
-	{
-		output = (output * 10) + (*str - '0');
-		str++;
-	}
-	return (output * pos_sign);
-}
-
-void	choose_stack(t_stack_node *stack)
-{
-	int				median;
-	int				i;
-	int				length;
-	t_stack_node	*current;
-
-	if (!stack)
+	if (!src || !*src)
 		return ;
-	length = stack_length(stack);
-	median = length / 2;
-	i = 0;
-	current = stack;
-	while (current)
-	{
-		current->index = i;
-		if (i <= median)
-			current->above_median = true;
-		else
-			current->above_median = false;
-		current = current->fwd;
-		i++;
-	}
+	number_to_push = *src;
+	*src = number_to_push->fwd;
+	if (*src)
+		(*src)->bwd = NULL;
+	number_to_push->fwd = *dst;
+	if (*dst)
+		(*dst)->bwd = number_to_push;
+	*dst = number_to_push;
 }
 
-int	stack_length(t_stack_node *head)
+void	pa(t_stack_node **a, t_stack_node **b)
 {
-	int				length;
-	t_stack_node	*current;
+	push_stack(a, b);
+	write(1, "pa\n", 3);
+}
 
-	current = head;
-	length = 0;
-	while (current != NULL)
-	{
-		length++;
-		current = current->fwd;
-	}
-	return (length);
+void	pb(t_stack_node **a, t_stack_node **b)
+{
+	push_stack(b, a);
+	write(1, "pb\n", 3);
+}
+
+void	swap(t_stack_node **head)
+{
+	t_stack_node	*first;
+	t_stack_node	*second;
+
+	if (!head || !*head || !(*head)->fwd)
+		return ;
+	first = *head;
+	second = first->fwd;
+	*head = second;
+	first->fwd = second->fwd;
+	if (second->fwd)
+		second->fwd->bwd = first;
+	second->fwd = first;
+	second->bwd = NULL;
+	first->bwd = second;
+}
+
+void	sa(t_stack_node **a)
+{
+	swap(a);
+	write(1, "sa\n", 3);
 }
